316425a5b6c9deb212553e650d26553cb4cab514
v-math <v-math@foxmail.com>
2023-02-15 21:53:10 +0800

    leetcode 刷题测试/

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author   : v-math
+# @Description: briefly describe module function.
+# @Time     : 2023/2/15 21:43
+# @File     : main.py
+# @Project  : leetcode
+
+
+if __name__ == '__main__':
+    print("Hello world.")
